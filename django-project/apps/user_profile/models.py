343ede2564072f2caf15298a0cdcd83c3a967a51
MM10 <mm33007@outlook.com>
2023-05-18 21:20:59 +0330

    created wishlist userprofile

--- django-project/apps/user_profile/models.py
+++ django-project/apps/user_profile/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+from apps.orders.countries import Countries
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address_line_1 = models.CharField(max_length=255, default='')
+    address_line_2 = models.CharField(max_length=255, default='')
+    city = models.CharField(max_length=255, default='')
+    state_province_region = models.CharField(max_length=255, default='')
+    zipcode = models.CharField(max_length=20, default='')
+    phone = models.CharField(max_length=255, default='')
+    country_region = models.CharField(
+        max_length=255, choices=Countries.choices, default=Countries.Canada)
+
+    def __str__(self):
+        return self.user
