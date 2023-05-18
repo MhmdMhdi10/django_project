4992ff010e57df63fc58090263d9fc6713e9a376
MM10 <mm33007@outlook.com>
2023-05-18 21:21:31 +0330

    created shipping models

--- django-project/apps/shipping/models.py
+++ django-project/apps/shipping/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Shipping(models.Model):
+    class Meta:
+        verbose_name = 'Shipping'
+        verbose_name_plural = 'Shipping'
+
+    name = models.CharField(max_length=255, unique=True)
+    time_to_delivery = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
