71609477e93364e7443a19bf221b6b32eef0d451
MM10 <mm33007@outlook.com>
2023-05-22 13:32:20 +0330

    fixed category models

--- django-project/apps/category/models.py
+++ django-project/apps/category/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 
+from apps.core_app.models import BaseModel
 
-class Category(models.Model):
+
+class Category(BaseModel):
     class Meta:
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
 
-    parent = models.ForeignKey('self', related_name='children', on_delete=models.CASCADE, blank=True, null=True)
     name = models.CharField(max_length=255, unique=True)
-
-    def __str__(self):
-        return self.name
+    parent = models.ForeignKey('self', related_name='children', on_delete=models.CASCADE, null=True, blank=True)
 
