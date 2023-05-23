3f65ef9d30c4fb4f805bd14e64751ff1703506a3
MM10 <mm33007@outlook.com>
2023-05-23 10:48:00 +0330

    fixed order models and added to admin

--- django-project/apps/orders/models.py
+++ django-project/apps/orders/models.py
@@ -1,15 +1,17 @@
 from django.db import models
-
-from django.db import models
-from apps.product.models import Product
-
-from datetime import datetime
 from django.contrib.auth import get_user_model
+from apps.core_app.models import BaseModel
+from apps.coupons.models import Coupon
+from apps.product.models import Product
 
 User = get_user_model()
 
 
-class Order(models.Model):
+class Order(BaseModel):
+    class Meta:
+        verbose_name = "order"
+        verbose_name_plural = "orders"
+
     class OrderStatus(models.TextChoices):
         not_processed = 'not_processed'
         processed = 'processed'
@@ -17,36 +19,41 @@ class Order(models.Model):
         delivered = 'delivered'
         cancelled = 'cancelled'
 
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    coupon = models.OneToOneField(Coupon, on_delete=models.SET_NULL, blank=True, null=True)
+    transaction_id = models.CharField(max_length=255, unique=True)
+    full_name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    discount_price = models.DecimalField(max_digits=5, decimal_places=2)
+    shipping_name = models.CharField(max_length=255)
+    shipping_price = models.DecimalField(max_digits=5, decimal_places=2)
+    shipping_time = models.CharField(max_length=255)
     status = models.CharField(
         max_length=50, choices=OrderStatus.choices, default=OrderStatus.not_processed)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    transaction_id = models.CharField(max_length=255, unique=True)
-    amount = models.DecimalField(max_digits=5, decimal_places=2)
-    full_name = models.CharField(max_length=255)
-    address_line_1 = models.CharField(max_length=255)
-    address_line_2 = models.CharField(max_length=255, blank=True)
-    city = models.CharField(max_length=255)
-    state_province_region = models.CharField(max_length=255)
-    postal_zip_code = models.CharField(max_length=20)
-    country_region = models.CharField(
-        max_length=255)     # choices=Countries.choices, default=Countries.Peru
-    telephone_number = models.CharField(max_length=255)
-    shipping_name = models.CharField(max_length=255)
-    shipping_time = models.CharField(max_length=255)
-    shipping_price = models.DecimalField(max_digits=5, decimal_places=2)
-    date_issued = models.DateTimeField(default=datetime.now)
-
-    def __str__(self):
-        return self.transaction_id
 
 
-class OrderItem(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
+class OrderItem(BaseModel):
+    class Meta:
+        verbose_name = 'Order Item'
+        verbose_name_plural = 'Order Items'
+
+    class OrderItemStatus(models.TextChoices):
+        not_processed = 'not_processed'
+        processed = 'processed'
+        shipping = 'shipped'
+        delivered = 'delivered'
+        cancelled = 'cancelled'
+
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=5, decimal_places=2)
+    discount_price = models.DecimalField(max_digits=5, decimal_places=2)
     count = models.IntegerField()
-    date_added = models.DateTimeField(default=datetime.now)
+    status = models.CharField(
+        max_length=50, choices=OrderItemStatus.choices, default=Order.OrderStatus.not_processed)
 
     def __str__(self):
         return self.name
