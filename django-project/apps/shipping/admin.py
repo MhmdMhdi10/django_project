d6ef95527e7aad7d2a6399a2364c6710aa69dc6c
MM10 <mm33007@outlook.com>
2023-05-23 11:04:29 +0330

    fixed shipping and user admins

--- django-project/apps/shipping/admin.py
+++ django-project/apps/shipping/admin.py
@@ -4,7 +4,6 @@ from apps.core_app.admin import BaseAdmin
 from.models import Shipping
 
 
-@admin.register(Shipping)
 class ShippingAdmin(BaseAdmin):
     list_display = ('id', 'name', 'price',
                     'created', 'last_updated', 'deleted_at', 'restored_at', 'is_deleted', 'is_active')
@@ -14,3 +13,4 @@ class ShippingAdmin(BaseAdmin):
     list_per_page = 25
 
 
+admin.site.register(Shipping, ShippingAdmin)
--- django-project/apps/user/admin.py
+++ django-project/apps/user/admin.py
@@ -1,13 +1,37 @@
 from django.contrib import admin
 
+from . import models
+
 from django.contrib.auth import get_user_model
+
+from apps.core_app.admin import BaseAdmin
 
 User = get_user_model()
 
 
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'email', 'is_staff', 'is_superuser', 'is_active', 'last_login')
-    list_display_links = ('first_name', 'last_name', 'email',)
-    search_fields = ('first_name', 'last_name', 'email', 'is_staff', 'is_superuser', 'is_active', 'last_login')
+class UserAdmin(BaseAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'email', 'is_active', 'is_staff', 'is_superuser',)
+    list_display_links = ('email',)
+    search_fields = ('email',)
     list_per_page = 25
+    filter_horizontal = ['groups', 'user_permissions']
+
+    fieldsets = [
+        ['Main', {'fields': ['first_name', 'last_name', 'email', 'password']}],
+        ['Permissions',
+         {'fields': ('is_active', 'is_superuser', 'is_staff', 'last_login', 'groups', 'user_permissions')}],
+    ]
+
+    add_fieldsets = [
+        ['Main', {'fields': ['first_name', 'last_name', 'email', 'password']}],
+    ]
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        is_superuser = request.user.is_superuser
+        if not is_superuser:
+            form.base_fields['is_superuser'].disabled = True
+        return form
+
+
+admin.site.register(User, UserAdmin)
