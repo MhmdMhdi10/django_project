ec18e71ec567c53279bb0a7cadabccb50228e6ee
MM10 <mm33007@outlook.com>
2023-05-23 11:04:13 +0330

    fixed review models

--- django-project/apps/reviews/models.py
+++ django-project/apps/reviews/models.py
@@ -9,15 +9,18 @@ from django.conf import settings
 User = settings.AUTH_USER_MODEL
 
 
-class Review(models.Model):
+class Review(BaseModel):
+    class Meta:
+        verbose_name = 'Review'
+        verbose_name_plural = 'Reviews'
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    reply = models.ForeignKey('self', on_delete=models.CASCADE, related_name='r_comments', blank=True, null=True)
+    reply = models.ForeignKey('self', on_delete=models.CASCADE, related_name='rcomments', blank=True, null=True)
     is_reply = models.BooleanField(default=False)
     head = models.CharField(max_length=255)
     body = models.TextField(max_length=400)
     rating = models.DecimalField(max_digits=2, decimal_places=1)
 
     def __str__(self):
-        return self.comment
-
+        return self.head
