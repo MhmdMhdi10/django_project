f91bd97fd1901f61acc99df19f11af42f9764a4e
MM10 <mm33007@outlook.com>
2023-05-25 17:54:04 +0330

    created activate account

--- django-project/src/redux/actions/auth.js
+++ django-project/src/redux/actions/auth.js
@@ -1,45 +1,99 @@
-import { SIGNUP_SUCCESS, SIGNUP_FAIL } from "./types";
+import {
+    SIGNUP_SUCCESS, SIGNUP_FAIL,
+    ACTIVATION_SUCCESS, ACTIVATION_FAIL,
+    SET_AUTH_LOADING, REMOVE_AUTH_LOADING,
+} from "./types";
 import axios from "axios";
 
 export const signup = (first_name, last_name, email, password, re_password) => async (dispatch) => {
 
-
-
-
-
-  const headers = {
-    "Content-Type": "application/json",
-  };
-
-  console.log(headers)
-
-  const body = JSON.stringify({
-    first_name,
-    last_name,
-    email,
-    password,
-    re_password
-  });
-
-  try {
-    const res = await axios.post(`${process.env.REACT_APP_API_URL}/auth/users`, body, {
-      headers,
-    });
-
-    if (res.status === 201) {
-      dispatch({
-        type: SIGNUP_SUCCESS,
-        payload: res.data,
-      });
-   } else {
-      dispatch({
-        type: SIGNUP_FAIL,
-      });
-    }
-  } catch (err) {
     dispatch({
-      type: SIGNUP_FAIL,
+       type: SET_AUTH_LOADING
     });
-  }
+
+    const headers = {
+        "Content-Type": "application/json",
+    };
+
+    console.log(headers)
+
+    const body = JSON.stringify({
+        first_name,
+        last_name,
+        email,
+        password,
+        re_password
+    });
+
+    try {
+        const res = await axios.post(`${process.env.REACT_APP_API_URL}/auth/users`, body, {
+            headers,
+        });
+
+        if (res.status === 201) {
+            dispatch({
+                type: SIGNUP_SUCCESS,
+                payload: res.data,
+            });
+        } else {
+            dispatch({
+                type: SIGNUP_FAIL,
+            });
+        }
+        dispatch({
+            type: REMOVE_AUTH_LOADING
+        });
+    } catch (err) {
+        dispatch({
+            type: SIGNUP_FAIL,
+        });
+        dispatch({
+            type: REMOVE_AUTH_LOADING
+        });
+    }
 };
 
+export const activate = (uid, token) => async dispatch => {
+    dispatch({
+       type: SET_AUTH_LOADING
+    });
+
+
+
+    const config = {
+        headers:{
+            'Content-type': 'application/json'
+        }
+    };
+
+    const body = JSON.stringify({
+        uid,
+        token
+    });
+
+    try {
+        const res = await axios.post(`${process.env.REACT_APP_API_URL}/auth/users/activate`)
+
+        if (res.status === 204) {
+            dispatch({
+               type: ACTIVATION_SUCCESS
+            });
+        } else {
+            dispatch({
+               type: ACTIVATION_FAIL
+            });
+        }
+        dispatch({
+           type: REMOVE_AUTH_LOADING
+        });
+
+    } catch (err){
+        dispatch({
+            type: ACTIVATION_FAIL
+        });
+        dispatch({
+           type: REMOVE_AUTH_LOADING
+        });
+    }
+
+};
--- django-project/src/redux/reducers/auth.js
+++ django-project/src/redux/reducers/auth.js
@@ -1,4 +1,8 @@
-import {SIGNUP_FAIL, SIGNUP_SUCCESS} from "../actions/types";
+import {
+    SIGNUP_FAIL, SIGNUP_SUCCESS,
+    ACTIVATION_SUCCESS,ACTIVATION_FAIL,
+    SET_AUTH_LOADING,REMOVE_AUTH_LOADING,
+    } from "../actions/types";
 
 const initialState = {
     access: localStorage.getItem('access'),
@@ -12,6 +16,22 @@ const initialState = {
 export default function (state = initialState, action) {
     const { type, payload} = action;
     switch (type){
+        case SET_AUTH_LOADING:
+            return {
+                ...state,
+                loading: true
+            }
+        case REMOVE_AUTH_LOADING:
+            return {
+                ...state,
+                loading: false
+            }
+
+        case ACTIVATION_SUCCESS:
+        case ACTIVATION_FAIL:
+            return {
+                ...state
+            }
         case SIGNUP_SUCCESS:
         case SIGNUP_FAIL:
             localStorage.removeItem('access')
