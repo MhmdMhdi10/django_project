6b0db26f07daff541fd850f8beebc2e1f0cb0ef2
MM10 <mm33007@outlook.com>
2023-06-02 03:10:27 +0330

    fixed some bugs

--- django-project/src/containers/pages/productDetail.jsx
+++ django-project/src/containers/pages/productDetail.jsx
@@ -275,7 +275,7 @@ const ProductDetail = ({
                                         <button
                                             onClick={addToCart}
                                             className="max-w-xs flex-1 bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-indigo-500 sm:w-full">
-                                            Agregar al Carrito
+                                            ADD to Cart
                                         </button>}
 
                                     <WishlistHeart
