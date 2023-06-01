4ad1d2a990cacc79d45d1417dddf5ac9aa1ff7b3
MM10 <mm33007@outlook.com>
2023-06-01 23:23:57 +0330

    added helpers

---
+++ django-project/src/helpers/fixedCountries.js
@@ -0,0 +1,27 @@
+export const countries = [
+    {
+        name: 'Tehran'
+    },
+    {
+        name: 'Mashhad'
+    },
+    {
+        name: 'Isfahan'
+    },
+    {
+        name: 'Shiraz'
+    },
+    {
+        name: 'Tabriz'
+    },
+    {
+        name: 'Ahvaz'
+    },
+    {
+        name: 'Qom'
+    },
+    {
+        name: 'Kerman'
+    }
+
+];
