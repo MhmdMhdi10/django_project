18cae05412755e6ce25717ed2df691c0506a616e
MM10 <mm33007@outlook.com>
2023-06-01 20:30:42 +0330

    added views to product

--- django-project/apps/product/tests.py
+++ django-project/apps/product/tests.py
@@ -1,49 +1,38 @@
-import pytest
-from django.core.files.uploadedfile import SimpleUploadedFile
-from apps.category.models import Category
+from django.test import TestCase
 from .models import Product
-from datetime import datetime
+from ..category.models import Category
 
 
-@pytest.fixture
-def test_category():
-    return Category.objects.create(name='Test Category')
+class ProductTestCase(TestCase):
+    def setUp(self):
+        category = Category.objects.create(
+            name='category'
+        )
+        Product.objects.create(
+            name='product1',
+            photo='photo1',
+            description='description1',
+            price=100,
+            discount_price=200,
+            category=category,
+            count=1,
+            sold=1,
+        )
 
+    def test_product(self):
+        category = Category.objects.get(name='category')
+        product = Product.objects.get(name='product1')
+        self.assertEqual(product.photo, 'photo1')
+        self.assertEqual(product.description, 'description1')
+        self.assertEqual(product.price, 100)
+        self.assertEqual(product.discount_price, 200)
+        self.assertEqual(product.category, category)
+        self.assertEqual(product.count, 1)
+        self.assertEqual(product.sold, 1)
+        self.assertNotEqual(product.photo, 'photo2')
+        self.assertNotEqual(product.description, 'description2')
+        self.assertNotEqual(product.price, 200)
 
-@pytest.fixture
-def test_product():
-    photo = SimpleUploadedFile(
-        name='test_product_photo.jpg',
-        content=open('test_product_photo.jpg', 'rb').read(),
-        content_type='image/jpeg'
-    )
-    return Product.objects.create(
-        name='Test Product',
-        photo=photo,
-        description='Test description',
-        price=10.99,
-        compare_price=9.99,
-        category=test_category(),
-        quantity=10,
-        sold=0,
-        date_created=datetime.now()
-    )
-
-
-def test_product_creation(test_product):
-    assert test_product.name == 'Test Product'
-    assert test_product.photo.name == 'photos/{}/{}.jpg'.format(datetime.now().strftime('%Y'),
-                                                                datetime.now().strftime('%m'))
-    assert test_product.description == 'Test description'
-    assert test_product.price == 10.99
-    assert test_product.compare_price == 9.99
-    assert test_product.category.name == 'Test Category'
-    assert test_product.quantity == 10
-    assert test_product.sold == 0
-    assert isinstance(test_product.date_created, datetime)
-    assert str(test_product) == 'Test Product'
-
-
-def test_get_thumbnail(test_product):
-    assert test_product.get_thumbnail() == '/media/photos/{}/{}.jpg'.format(datetime.now().strftime('%Y'),
-                                                                            datetime.now().strftime('%m'))
+    def test_str(self):
+        product = Product.objects.get(name='product1')
+        self.assertEqual(product.__str__(), 'product1')
