7bdbea07c946ea48d8a1f12577eefef6e99a312d
MM10 <mm33007@outlook.com>
2023-06-01 22:22:32 +0330

    added cart to redux

--- django-project/src/redux/reducers/index.js
+++ django-project/src/redux/reducers/index.js
@@ -2,9 +2,12 @@ import { combineReducers } from 'redux';
 import Auth from './auth';
 import Alert from "./alert";
 import Categories from "./categories";
+import Cart from './cart';
+
 
 export default combineReducers({
     Auth,
     Alert,
-    Categories
+    Categories,
+    Cart
 })
